9eebf5e838acc2db9e8f88f5681a8c953ba56c6a
TheRealMarrk <matheuscarvalho98@outlook.com.br>
2023-01-31 16:56:00 -0300

    Create SignUp page

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -3,27 +3,29 @@ import { Container, Form, Background } from "./styles";
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 
-import { FiLock, FiMail } from "react-icons/fi";
+import { FiLock, FiMail, FiUser } from "react-icons/fi";
 
 
-export function SignIn(){
+export function SignUp(){
   return(
     <Container>
+      <Background />
+
       <Form>
         <h1>RocketNotes</h1>
         <p>Aplicação para organizar e gerenciar seus links úteis</p>
-        <h2>Faça seu login</h2>
+        <h2>Crie sua conta</h2>
 
-        <Input placeholder="Digite seu e-mail" type="text" icon={FiMail}/>
-        <Input placeholder="Digite sua senha" type="password" icon={FiLock}/>
+        <Input placeholder="Nome" type="text" icon={FiUser}/>
+        <Input placeholder="Email" type="email" icon={FiMail}/>
+        <Input placeholder="Senha" type="password" icon={FiLock}/>
 
-        <Button title="Entrar"/>
+        <Button title="Cadastrar"/>
         <a href="http://" target="_blank" rel="noopener noreferrer">
-          Criar conta
+          Voltar para o login
         </a>
       </Form>
 
-      <Background />
 
     </Container>
     )
